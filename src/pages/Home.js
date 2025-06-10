cfed4541371ccb12ad2a04d3ccf18939ce05fd66
MrAngelHuerta <koriaanonimo@gmai.com>
2025-06-09 19:18:29 -0600

    cambios

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -32,7 +32,6 @@ const Home = () => {
       </nav>
 
       <div className="container mt-4">
-
         {/* Buscador */}
         <form className="mb-4" onSubmit={handleSearch}>
           <div className="input-group">
@@ -91,6 +90,20 @@ const Home = () => {
           </div>
         </section>
       </div>
+
+      {/* Footer */}
+      <footer className="footer">
+        <div className="footer-content">
+          <h3>Contáctanos</h3>
+          <p>Email: contacto@miots.com</p>
+          <p>Teléfono: +123 456 789</p>
+          <div className="social-links">
+            <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">Facebook</a>
+            <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">Twitter</a>
+            <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">Instagram</a>
+          </div>
+        </div>
+      </footer>
     </>
   );
 };
